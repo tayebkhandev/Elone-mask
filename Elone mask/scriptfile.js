6857f38d7cf6a62ec777b1360b642caa1cace47b
muhammad tayeb <tayebkhan1155@gmail.com>
2023-01-30 13:32:24 +0500

    21/2/2021

--- Elone mask/scriptfile.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll',function () {
-    let navbar = this.document.getElementById("#navbar");
-    if (window.pageYOFFset >= 10) {
-        navbar.classList.add('skicky');
-    }
-});
